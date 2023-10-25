6277820e2c2cb9bb44ec414dcd60bd675c0eb15c
Geoffrey Woollard <geoffwoollard@gmail.com>
2023-10-25 10:50:58 -0700

    autograder test.sh

--- _tests/assignment6/test.sh
+++ _tests/assignment6/test.sh
@@ -1,10 +1,10 @@
 hdir=$(pwd)
-for zipped in $(ls *_ASSIGN5_CPSC110.zip); do
+for zipped in $(ls *_ASSIGN6_CPSC110.zip); do
     echo '----------------------------------------'
     echo 'testing' $zipped
     unzip $zipped
     cd $(basename -s .zip $zipped)
-    for fname in test_easy.py test_insert.py test_stats.py; do
+    for fname in test_max.py test_rewrite.py test_DNA.py; do
         cp ../$fname .
         python $fname
     done
