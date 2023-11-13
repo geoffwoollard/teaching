56f5179603c3087d93f8374bcb63baaea877cdd3
geoffwoollard <geoffwoollard+github@gmail.com>
2023-11-12 17:46:05 -0800

    Update test.sh

--- _tests/assignment7/test.sh
+++ _tests/assignment7/test.sh
@@ -4,7 +4,7 @@ for zipped in $(ls *_ASSIGN7_CPSC110.zip); do
     echo 'testing' $zipped
     unzip $zipped -d $(basename -s .zip $zipped)
     cd $(basename -s .zip $zipped)
-    for fname in test_easy.py test_insert.py test_stats.py; do
+    for fname in test_*.py; do
         cp ../$fname .
         python $fname
     done
